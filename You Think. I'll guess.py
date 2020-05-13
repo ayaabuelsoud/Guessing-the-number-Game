0047ebc95c0ca157e80012447ea68e974f8eab7c
Aya <47188673+ayaabuelsoud@users.noreply.github.com>
2020-05-13 22:48:48 +0200

    Update You Think. I'll guess.py

--- You Think. I'll guess.py
+++ You Think. I'll guess.py
@@ -8,7 +8,7 @@ print("Hello! What is your name?")
 username = input()
 
 #the range of numbers to guess from
-print(username + " ,Please Think of a number, and i will guess it")
+print(username + ", Please Think of a number, and i will guess it")
 print("Now, Tell me the range of numbers to guess from")
 low = int(input("the minimum is: "))
 high = int(input("the maximum is: "))
@@ -19,7 +19,7 @@ state = True
 
 while state :
     print("Is your secret number is " + str(medium)+ "?!")
-    guess = input("Enter 'H' if it was too high./nEnter 'l' if it was too low./n Enter'c' if it was correct :")
+    guess = input("Enter 'H' if it was too high.\nEnter 'l' if it was too low.\nEnter 'c' if it was correct :")
     if guess =='c' :
         print("Game over, your secrert number is "+ str(medium))
         state = False
